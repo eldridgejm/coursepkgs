b4af38bacaf456a8b07f9901ddd17ef3254bc31d
Justin Eldridge <eldridgejm@gmail.com>
2022-12-20 19:19:30 -0600

    Added removesoln.

--- flake.nix
+++ flake.nix
@@ -76,6 +76,30 @@
           propagatedBuildInputs = [ pkgs.python3Packages.notebook ];
         };
 
+        removesoln =
+          let
+            texsoup = pkgs.python3Packages.buildPythonPackage rec {
+              pname = "texsoup";
+              version = "0.3.1";
+              name = "${pname}-${version}";
+              src = builtins.fetchurl {
+                url = "https://files.pythonhosted.org/packages/84/58/1c503390ed1a81cdcbff811dbf7a54132994acef8dd2194d55cf657a9e97/TexSoup-0.3.1.tar.gz";
+                sha256 = "02xpvmhy174z6svpghzq4gs2bsyh0jxc2i7mark8ls73mg82lsrz";
+              };
+              doCheck = false;
+            };
+          in
+            pkgs.python3Packages.buildPythonPackage rec {
+              name = "removesoln";
+              src = pkgs.fetchFromGitHub {
+                owner = "eldridgejm";
+                repo = "removesoln";
+                rev = "02ce3d9a4d1a7bd9e76182ff796aea4d2d2ddb35";
+                sha256 = "sha256-EIVhIhfp9xsQIZNDEJcUnlW2bGTZYi4gJQLF/lFGaUs=";
+              };
+              propagatedBuildInputs = [ texsoup ];
+            };
+
       }
     );
 
