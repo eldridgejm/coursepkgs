4c41eac19ae5871956244af14b7334ab7afaad36
Justin Eldridge <eldridgejm@gmail.com>
2023-09-25 13:47:07 -0700

    Updated nixpkgs.

--- flake.nix
+++ flake.nix
@@ -2,8 +2,7 @@
   description = "Packages used in DSC courses @ UCSD.";
 
   inputs = {
-
-    nixpkgs.url = github:NixOS/nixpkgs/21.11;
+    nixpkgs.url = github:NixOS/nixpkgs/23.05;
 
     automata-flake.url = "github:eldridgejm/automata";
     automata-flake.inputs.nixpkgs.follows = "nixpkgs";
@@ -19,25 +18,24 @@
 
     removesoln-flake.url = "github:eldridgejm/removesoln";
     removesoln-flake.inputs.nixpkgs.follows = "nixpkgs";
-
   };
 
   outputs = {
-    self, nixpkgs, automata-flake, dsc40graph-flake, gradelib-flake, dsctex-flake,
+    self,
+    nixpkgs,
+    automata-flake,
+    dsc40graph-flake,
+    gradelib-flake,
+    dsctex-flake,
     removesoln-flake,
-  }:
-  let
-    supportedSystems = [ "x86_64-linux" "x86_64-darwin" "aarch64-darwin" ];
+  }: let
+    supportedSystems = ["x86_64-linux" "x86_64-darwin" "aarch64-darwin"];
     forAllSystems = f: nixpkgs.lib.genAttrs supportedSystems (system: f system);
-
-  in
-  {
-    packages = forAllSystems (system: 
-    let
-      pkgs = import nixpkgs { system = "${system}"; };
-    in
-      rec {
-
+  in {
+    packages = forAllSystems (
+      system: let
+        pkgs = import nixpkgs {system = "${system}";};
+      in rec {
         automata = automata-flake.defaultPackage."${system}";
         dsc40graph = dsc40graph-flake.defaultPackage."${system}";
         gradelib = gradelib-flake.defaultPackage."${system}";
@@ -52,11 +50,9 @@
             url = "https://files.pythonhosted.org/packages/bf/71/d107b262e6a002f66369e42f22044f21d744353e76fd69ae3c0e47410b40/gradescope-utils-0.4.0.tar.gz";
             sha256 = "1733hb87gkr7cs9wdn8paql1jw37z3jszhr55x7yhjasvx7r293g";
           };
-          propagatedBuildInputs = [ pkgs.python3Packages.notebook ];
+          propagatedBuildInputs = [pkgs.python3Packages.notebook];
         };
-
       }
     );
-
   };
 }
